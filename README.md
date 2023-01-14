248fb377fd95254b2cb22b7a0a872cac5af08b49
Prem Patel <80719066+Prem-ium@users.noreply.github.com>
2023-01-13 19:57:49 -0500

    Implementing Environmental Variables

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# Utility-Bill-Automation
-A Python script that allows users to automate the boring parts within a Savannah GA's utility bill payment website. 
+
+# Savannah GA Utility Bill AutoPay
+
+Automate bill payment of the lengthy/boring Savannah GA Utility Bill Payment website across multiple different accounts using this Selenium Python script.
 
 
-## Project Dependencies
+## Installation
 
 
 ```pip
@@ -10,13 +12,12 @@ A Python script that allows users to automate the boring parts within a Savannah
 ```
 Addionally, you may be able to git clone this repository and 
 ```pip
-  git clone https://github.com/Prem-ium/Utility-Bill-Automation.git
+  git clone https://github.com/sazncode/Utility-Bill-Automation.git
   pip install -r requirements.txt
 ```
 
-Be aware your browser driver MUST be within your directory. 
+I despise Savannah GA GOV Utility Bill process. Manually having to type the same information multiple times for three seperate bills became exhausting, so I decided to automate it.
 
-## USE AT YOUR OWN RISK
-It is highly recommended to have HandlePayment and FullyAutomated turned OFF. In this case, you would need to manually input payment details and leave the boring stuff like filling out account number, barcode, payor phone number, name, address, etc to the bot. 
+## WARNING:
+Storing sensitive financial information such as debit/credit card or bank credentials is highly discouraged. If you must store financial information, it is recommended that you use caution and proceed at your own risk. The developer(s) of this program are not responsible for any financial loss incurred that may occur as a result of storing financial information in this program.
 
-This script can be used to pay off multiple utility accounts, if needed. 
--- main.py
+++ main.py
@@ -2,60 +2,128 @@
 # Python Automation Application
 
 # USE handlePayment AND fullyAutomated AT YOUR OWN RISK.
-# If you are insitent on using either, I HIGHLY RECOMMEND YOU USE A VIRTUAL DEBIT CARD OR A PRIVACY.COM TEMPORARY CARD.
-
+# If you are insistent on using either, I HIGHLY RECOMMEND YOU USE A VIRTUAL DEBIT CARD OR A PRIVACY.COM (https://privacy.com/join/G25UX) TEMPORARY CARD.
+import os
+import json
+from time import sleep
+from dotenv import load_dotenv
 from lib2to3.pgen2 import driver
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException, UnexpectedAlertPresentException
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    UnexpectedAlertPresentException
+)
 
-import time
-import secrets
+load_dotenv()
 
-ACCOUNTS = secrets.ACCOUNT_NUMBERS.split(',')
-BARCODES = secrets.BARCODES.split(',')
+if not os.environ["ACCOUNT_NUMBERS"]:
+    raise Exception("ACCOUNT_NUMBERS is not set in .env file. Please set it and try again.")
+elif not os.environ["BARCODES"]:
+    raise Exception("BARCODES is not set in .env file. Please set it and try again.")
+else:
+    ACCOUNTS = os.environ["ACCOUNT_NUMBERS"].split(',')
+    BARCODES = os.environ.get('BARCODES').split(',')
+
+if len(ACCOUNTS) != len(BARCODES):
+    raise Exception("ACCOUNT_NUMBERS and BARCODES are not the same length. Please make sure they are and try again.")
+
+if not os.environ["PAYOR_INFO"]:
+    raise Exception("PAYOR_INFO is not set in .env file. Please set it and try again.")
+else:
+    PAYOR_INFO = os.environ.get('PAYOR_INFO')
+    PAYOR_INFO = json.loads(PAYOR_INFO)
+HANDLE_PAYMENT = True if os.environ.get('HANDLE_PAYMENT', 'False').lower() == 'true' else False
+
+if HANDLE_PAYMENT:
+    PAYMENT_METHOD = os.environ.get('PAYMENT_METHOD').lower()
+    if PAYMENT_METHOD == 'bank':
+        BANK = os.environ.get('BANK')
+        BANK = json.loads(BANK)
+    elif PAYMENT_METHOD == 'credit':
+        CC_CARD = os.environ.get('CREDIT_CARD')
+        CC_CARD = json.loads(CC_CARD)
+    elif PAYMENT_METHOD == 'debit':
+        DB_CARD = os.environ.get('DEBIT_CARD')
+        DB_CARD = json.loads(DB_CARD)
+
+FULLY_AUTOMATED = True if os.environ.get('FULLY_AUTOMATED', 'False').lower() == 'true' else False
+
+def getDriver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--start-minimized")
+    try:
+        driver = webdriver.Chrome(
+                service=Service(ChromeDriverManager(cache_valid_range=30).install()),
+                options=chrome_options)
+    except:
+        driver = webdriver.Chrome()
+    return driver
 
 # Payment Method Functions
 def payWithCreditCard(driver):
     driver.find_element(By.XPATH, value ='//*[@id="category-CC"]/div[1]/div/label/span').click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, value ='//*[@id="ccAccountNumber"]').send_keys(secrets.cc_card['number'])
-    driver.find_element(By.XPATH, value ='//*[@id="ccCvv"]').send_keys(secrets.cc_card['ccv'])
-    Select(driver.find_element(By.ID, value='ccExpiryDateMonth')).select_by_value(secrets.cc_card['expirationMonth'])
-    Select(driver.find_element(By.ID, value='ccExpiryDateYear')).select_by_value(secrets.cc_card['expirationYear'])
-    driver.find_element(By.XPATH, value ='//*[@id="ccCardHolderName"]').send_keys(secrets.cc_card['cardholder'])
+    sleep(2)
+    driver.find_element(By.XPATH, value ='//*[@id="ccAccountNumber"]').send_keys(CC_CARD['number'])
+    driver.find_element(By.XPATH, value ='//*[@id="ccCvv"]').send_keys(CC_CARD['ccv'])
+    Select(driver.find_element(By.ID, value='ccExpiryDateMonth')).select_by_value(CC_CARD['expirationMonth'])
+    Select(driver.find_element(By.ID, value='ccExpiryDateYear')).select_by_value(CC_CARD['expirationYear'])
+    driver.find_element(By.XPATH, value ='//*[@id="ccCardHolderName"]').send_keys(CC_CARD['cardholder'])
 
 def payWithDebitCard(driver):
     driver.find_element(By.XPATH, value ='//*[@id="category-DC"]/div[1]/div/label/span').click()
-    time.sleep(2)
-    driver.find_element(By.ID, value ='dcAccountNumber').send_keys(secrets.db_card['number'])
-    driver.find_element(By.ID, value ='dcCvv').send_keys(secrets.db_card['ccv'])
-    Select(driver.find_element(By.ID, value='dcExpiryDateMonth')).select_by_value(secrets.db_card['expirationMonth'])
-    Select(driver.find_element(By.ID, value='dcExpiryDateYear')).select_by_value(secrets.db_card['expirationYear'])
-    driver.find_element(By.ID, value ='dcCardHolderName').send_keys(secrets.db_card['cardholder'])
+    sleep(2)
+    driver.find_element(By.ID, value ='dcAccountNumber').send_keys(DB_CARD['number'])
+    driver.find_element(By.ID, value ='dcCvv').send_keys(DB_CARD['ccv'])
+    Select(driver.find_element(By.ID, value='dcExpiryDateMonth')).select_by_value(DB_CARD['expirationMonth'])
+    Select(driver.find_element(By.ID, value='dcExpiryDateYear')).select_by_value(DB_CARD['expirationYear'])
+    driver.find_element(By.ID, value ='dcCardHolderName').send_keys(DB_CARD['cardholder'])
 
 def payWithBank(driver):
     # Bank Account 
     driver.find_element(By.XPATH, value ='//*[@id="category-DD"]/div[1]/div/label/span').click()
-    time.sleep(2)
+    sleep(2)
+    try:
+        if (BANK['Type'] == 'Checking'):
+            driver.find_element(By.XPATH, value ='//*[@id="hide-radio-pm-dd-1"]/div/div[2]/div/fieldset/div/label/span').click()
+        elif (BANK['Type'] == 'Savings'):
+            driver.find_element(By.XPATH, value ='//*[@id="hide-radio-pm-dd-1"]/div/div[2]/div/fieldset/div[2]/label/span').click()
+    except:
+        try:
+            if (BANK['Type'] == 'Checking'):
+                driver.find_element(By.XPATH, value = "//button[contains(.,'Checking')]").click()
+            elif (BANK['Type'] == 'Savings'):
+                driver.find_element(By.XPATH, value = "//button[contains(.,'Savings')]").click()
+        except:
+            pass
+    driver.find_element(By.XPATH, value ='//*[@id="ddBankName"]').send_keys(BANK['Banker'])
+    driver.find_element(By.XPATH, value ='//*[@id="ddAccountHolderName"]').send_keys(BANK['Holder'])
+    try:
+        driver.find_element(By.XPATH, value ='/html/body/div[6]/div[1]/form/div[2]/div[1]/div[5]/fieldset/div[3]/div[2]/div/div[3]/div[1]/input').send_keys(BANK['Routing'])
+    except:
+        try:
+            sleep(5)
+            driver.find_element(By.XPATH, value = '//*[@id="ddRoutingNumber"]').send_keys(BANK['Routing'])
+        except:
+            pass
+    driver.find_element(By.XPATH, value='//*[@id="ddAccountNumber"]').send_keys(BANK['Account'])
+    driver.find_element(By.XPATH, value='//*[@id="ddAccountNumber2"]').send_keys(BANK['Account'])
 
-    if (secrets.bank['Type'] == 'Checking'):
-        driver.find_element(By.XPATH, value ='//*[@id="hide-radio-pm-dd-1"]/div/div[2]/div/fieldset/div/label/span').click()
-    elif (secrets.bank['Type'] == 'Savings'):
-        driver.find_element(By.XPATH, value ='//*[@id="hide-radio-pm-dd-1"]/div/div[2]/div/fieldset/div[2]/label/span').click()
-    
-    driver.find_element(By.XPATH, value ='//*[@id="ddBankName"]').send_keys(secrets.bank['Banker'])
-    driver.find_element(By.XPATH, value ='//*[@id="ddAccountHolderName"]').send_keys(secrets.bank['Holder'])
-    driver.find_element(By.XPATH, value ='/html/body/div[6]/div[1]/form/div[2]/div[1]/div[5]/fieldset/div[3]/div[2]/div/div[3]/div[1]/input').send_keys(secrets.bank['Routing'])
-    driver.find_element(By.XPATH, value='//*[@id="ddAccountNumber"]').send_keys(secrets.bank['Account'])
-    driver.find_element(By.XPATH, value='//*[@id="ddAccountNumber2"]').send_keys(secrets.bank['Account'])
+def automate(account, barcode):
+    # URL of the bill website to be scraped
+    url = 'https://revenue.savannahga.gov/revwebpayub/default.aspx'
 
-def automate(driver, account, barcode):
+    driver = getDriver()
+    driver.get(url)
+
     # Barcode/Account Number Page
     driver.find_element(By.ID, value ='objWP_epayment_ESearchManager1_Web_CO_SearchPanel1_txt_GFD0').send_keys(account)
     driver.find_element(By.ID, value ='objWP_epayment_ESearchManager1_Web_CO_SearchPanel1_txt_GFD1').send_keys(barcode)
@@ -65,86 +133,86 @@ def automate(driver, account, barcode):
     if (driver.find_elements(By.XPATH, value ='//*[@id="objWP_epayment_ESearchManager1_vdsSummary"]/ul/li')):
         driver.execute_script('alert(\'Barcode is incorrect. Exiting...\');')
         print('Incorrect barcode provided. Exiting...')
-        time.sleep(10)
+        sleep(10)
         return
     elif (driver.find_elements(By.XPATH, value ='//*[@id="objWP_epayment_ESearchManager1_Web_CO_SearchPanel1_lblNoResult"]')):
         driver.execute_script('alert(\'No results found, please check account number and barcode are correct & run again upon account number/barcode correction. Exiting...\');')
         print('No results found, please check account number and barcode are correct. Exiting...')
-        time.sleep(15)
+        sleep(15)
         return
     print('Account number and barcode are correct.')
+    sleep(4)
     driver.find_element(By.XPATH, value ='/html/body/form/table/tbody/tr[2]/td[2]/table[2]/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr[5]/td/input[2]').click()
     try:
         if (driver.find_element(By.XPATH, value ='//*[@id="ctl02_grdAmount_ctl01_lblEmptyGrid"]').get_attribute('innerHTML') == 'No Outstanding Balance Found.'):
             driver.execute_script('alert(\'No outstanding balance found. Exiting...\');')
             print('No outstanding balance found. Exiting...')
-            time.sleep(10)
+            sleep(10)
             return
     except NoSuchElementException:
         print('An outstanding balance exists... Continuing...')
-    
+    sleep(3)
     # Next Step Page
     driver.find_element(By.XPATH, value='//*[@id="ctl02_hlinkNextStep"]').click()
     driver.find_element(By.XPATH, value='//*[@id="ctl02_hlinkNextStep"]').click()
+    sleep(3)
+
     # NEW PAYER INFO PAGE
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtFirstName"]').send_keys(secrets.PayerInfo['FirstName'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtLastName"]').send_keys(secrets.PayerInfo['LastName'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtCivic"]').send_keys(secrets.PayerInfo['HouseNumber'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtEmail"]').send_keys(secrets.PayerInfo['Email'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtStreet"]').send_keys(secrets.PayerInfo['Street'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtCity"]').send_keys(secrets.PayerInfo['City'])
-    Select(driver.find_element(By.ID, value='ctl02_ddlState')).select_by_visible_text(secrets.PayerInfo['State'])
-    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtZipCode"]').send_keys(secrets.PayerInfo['Zip'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtFirstName"]').send_keys(PAYOR_INFO['FirstName'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtLastName"]').send_keys(PAYOR_INFO['LastName'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtCivic"]').send_keys(PAYOR_INFO['HouseNumber'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtEmail"]').send_keys(PAYOR_INFO['Email'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtStreet"]').send_keys(PAYOR_INFO['Street'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtCity"]').send_keys(PAYOR_INFO['City'])
+    Select(driver.find_element(By.ID, value='ctl02_ddlState')).select_by_visible_text(PAYOR_INFO['State'])
+    driver.find_element(By.XPATH, value ='//*[@id="ctl02_txtZipCode"]').send_keys(PAYOR_INFO['Zip'])
     driver.find_element(By.XPATH, value ='//*[@id="ctl02_hlinkNextStep"]').click()
     driver.find_element(By.XPATH, value ='//*[@id="main-container"]/form/div/div[2]/div/input').click()
-
+    sleep(5)
     #Payment Page
-    driver.find_element(By.XPATH, value ='//*[@id="customer.dayPhone.formattedText"]').send_keys(secrets.PayerInfo['Phone'])
-    if secrets.handlePayment == True:
-        if(secrets.payment_method == 'bank'):
+    driver.find_element(By.XPATH, value ='//*[@id="customer.dayPhone.formattedText"]').send_keys(PAYOR_INFO['Phone'])
+    if HANDLE_PAYMENT == True:
+        if(PAYMENT_METHOD == 'bank'):
             payWithBank(driver)
-        elif (secrets.payment_method == 'credit'):
+        elif (PAYMENT_METHOD == 'credit'):
             payWithCreditCard(driver)
-        elif (secrets.payment_method == 'debit'):
+        elif (PAYMENT_METHOD == 'debit'):
             payWithDebitCard(driver)
     else:
         driver.execute_script("alert('Payment must be manually entered from this point. Handle Payment variable is False. Browser closes in 10 minutes.');")
-        time.sleep(600)
+        sleep(600)
         return
-    time.sleep(2)
+    sleep(2)
     # Submit 
     driver.find_element(By.XPATH, value ='//*[@id="main-container"]/form/div[2]/div[2]/div/input[1]').click()
 
-    if secrets.fullyAutomated:
-        time.sleep(6)
+    if FULLY_AUTOMATED:
+        sleep(6)
         # TERMS AND CONDITIONS AGREEMENT!!
         driver.find_element(By.XPATH, value ='//*[@id="main-container"]/form/div/div/div[6]/div/div/label/span').click()
         # FINAL SUBMIT BUTTON!!!
         driver.find_element(By.XPATH, value ='//*[@id="make-payment-btn"]').click()
     else:
         driver.execute_script("alert('Fully Automated variable is set to False. You must manually submit your payment. You have 5 minutes before the browser closes.');")
-        time.sleep(300)
+        sleep(300)
 
 # Main Program
 def main():
-    # URL of the bill website to be scraped
-    url = 'https://revenue.savannahga.gov/revwebpayub/default.aspx'
-    driver = webdriver.Chrome()
     try:
         i = 0
         for account in ACCOUNTS:
-            print(f'Now running for account: {account} and barcode: {BARCODES[i]}')
-            driver.get(url)
-            automate(driver, account, BARCODES[i])
-            time.sleep(2)
+            print(f'Starting:\n\tAccount: {account}\n\tBarcode: {BARCODES[i]}\n\tPayment Method: {PAYMENT_METHOD}\n\n')
+            automate(account, BARCODES[i])
+            sleep(2)
             i+=1
             print()
     except Exception as e:
         print(e)
         driver.execute_script("alert('Error occurred. Browser closes in 5 minutes.');")
-        time.sleep(300)
+        sleep(300)
         return
 
 if __name__ == '__main__':
     main()
-    time.sleep(3)
+    sleep(3)
+    
